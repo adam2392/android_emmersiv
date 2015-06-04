6a4434b987c7414cc227f98fc1569c713ea01b28
Adam Li <adam2392@gmail.com>
2015-06-04 12:01:59 -0700

    Finished adding edittext options.

--- app/src/main/java/adamli/omgandroid/MainActivity.java
+++ app/src/main/java/adamli/omgandroid/MainActivity.java
@@ -3,9 +3,9 @@ package adamli.omgandroid;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -13,32 +13,38 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     TextView mainTextView;
     Button mainButton;
+    EditText mainEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
         //1. Access textview defined in layout XML
         // and then alter its text
-        mainTextView = (TextView) findViewById(R.id.main_textview);
+//        mainTextView = (TextView) findViewById(R.id.main_textview);
         mainTextView.setText("Set in Java!!");
 
         //2. Access the button defined in layout xml
         // and then listen for it here
-        mainButton = (Button) findViewById(R.id.main_button);
+//        mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);    //MainActivity is the intended listener
+
+        //3. Access the edittext defined in layout xml
+//        mainEditText = (EditText) findViewById(R.id.main_edittext);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public void onClick(View v) {
-        mainTextView.setText("Button Pressed!!!!!! by Adam");
+//        mainTextView.setText("Button Pressed!!!!!! by Adam");
+        mainTextView.setText(mainEditText.getText().toString() + " is learning Android" +
+                "development!");
     }
 }
